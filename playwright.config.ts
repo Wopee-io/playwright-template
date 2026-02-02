01bdc42f0e297f30b989240f41f4d79f0fa6199e
Sota Giorgadze <117805980+fotyG@users.noreply.github.com>
2026-02-02 11:51:52 +0100

    feat: add dynamic playwright project config
    - Implement support of dynamic browser and device selection based on environment variables. 
    - Sync with agent's playwright config file.

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,65 @@
-import { defineConfig, devices } from "@playwright/test";
+import {
+  devices,
+  defineConfig,
+  PlaywrightTestConfig,
+  PlaywrightTestOptions,
+} from "@playwright/test";
+
+type BrowserName = "chromium" | "firefox" | "webkit";
+
+const browserToDevice: Record<BrowserName, keyof typeof devices> = {
+  chromium: "Desktop Chrome",
+  firefox: "Desktop Firefox",
+  webkit: "Desktop Safari",
+};
+
+const selectedBrowser =
+  (process.env.WOPEE_BROWSER as BrowserName) || "chromium";
+const browser = browserToDevice[selectedBrowser] ?? "Desktop Chrome";
+
+const projectsConfig: PlaywrightTestConfig<PlaywrightTestOptions>[] = [];
+
+if (process.env.VIEWPORT) {
+  const [width, height] = process.env.VIEWPORT.toLowerCase()
+    .split("x")
+    .map(Number);
+  projectsConfig.push({
+    name: `${browser} (custom viewport)`,
+    use: {
+      ...devices[browser],
+      viewport: { width, height },
+      headless: true,
+    },
+  });
+} else if (process.env.WOPEE_DEVICE) {
+  const deviceKey = process.env.WOPEE_DEVICE as keyof typeof devices;
+  const device = devices[deviceKey];
+
+  let browserType: BrowserName | null =
+    (process.env.WOPEE_BROWSER as BrowserName | null) || null;
+  // Firefox does not support isMobile
+  if ((device.isMobile || device.hasTouch) && browserType === "firefox")
+    browserType = null;
+
+  projectsConfig.push({
+    name: `${deviceKey}${browser ? ` (${browserType})` : ""}`,
+    use: {
+      ...device,
+      ...(browserType && {
+        defaultBrowserType: browserType,
+      }),
+      headless: true,
+    },
+  });
+} else {
+  projectsConfig.push({
+    name: `${browser} (default)`,
+    use: { 
+      ...devices[browser], 
+      headless: true,
+    },
+  });
+}
 
 export default defineConfig({
   testDir: ".",
@@ -19,14 +80,5 @@ export default defineConfig({
     },
   },
 
-  projects: [
-    {
-      name: "chrome",
-      use: {
-        headless: true,
-        viewport: { width: 1600, height: 1000 },
-        channel: "chrome",
-      },
-    },
-  ],
+  projects: projectsConfig,
 });
